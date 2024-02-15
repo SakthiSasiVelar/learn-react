5735041434d0190061eac1104871d6104eff608a
unknown <sakthisasivelar@gmail.com>
2024-02-15 16:20:01 +0530

    day 1 of learning react

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading = React.createElement(
+  "h1",
+  {
+    id: "heading",
+  },
+  "Day 1 of react learning"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);

1ade91d5f86686077f6704aa7a81cde0bffc0e1d
Sakthi sasi velar <sakthisasivelar@gmail.com>
2024-04-27 15:24:20 +0530

    created app layout

--- src/components/HeaderComponent.js
+++
@@ -1,9 +0,0 @@
-const HeaderComponent = () => {
-  return (
-    <div style={{backgroundColor : "red"}}>
-      <h1 style={{textAlign:"center"}}>Header</h1>
-    </div>
-  )
-}
-
-export default HeaderComponent;

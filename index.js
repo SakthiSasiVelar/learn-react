5dbf672ec9329c9eea4913e110f555e3f24decb8
Sakthi sasi velar <sakthisasivelar@gmail.com>
2024-04-27 13:14:12 +0530

    introduction to components

---
+++ index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client';
+import HeaderComponent from './src/components/HeaderComponent';
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeaderComponent />);
---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,9 @@
+const HeaderComponent = () => {
+  return (
+    <div style={{backgroundColor : "red"}}>
+      <h1 style={{textAlign:"center"}}>Header</h1>
+    </div>
+  )
+}
+
+export default HeaderComponent;
